79e40389a0d9173bb3f3e528736162ff22f7b61e
Kimmo Forss <kimmo.forss@microsoft.com>
2021-01-29 22:05:27 +0200

    ISCSI boot diagnostics error

--- deploy/terraform/terraform-units/modules/sap_landscape/iscsi.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/iscsi.tf
@@ -102,8 +102,8 @@ resource "azurerm_linux_virtual_machine" "iscsi" {
 
   boot_diagnostics {
     storage_account_uri = length(var.diagnostics_storage_account.arm_id) > 0 ? (
-      azurerm_storage_account.storage_bootdiag[0].primary_blob_endpoint) : (
-      data.azurerm_storage_account.storage_bootdiag[0].primary_blob_endpoint
+      data.azurerm_storage_account.storage_bootdiag[0].primary_blob_endpoint) : (
+      azurerm_storage_account.storage_bootdiag[0].primary_blob_endpoint
     )
   }
 
